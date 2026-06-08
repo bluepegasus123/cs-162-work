6feb0b341478fd3a613d4bb5fba620863195f6d3
William Louis <bluepegasus123@gmail.com>
2026-06-08 07:55:38 +0000

    refactor and pipes are working

--- hw-shell/shell.c
+++ hw-shell/shell.c
@@ -214,6 +214,180 @@ int runProgram(unused struct tokens* tokens) {
   return 1;
 }
 
+pid_t execProgramSegmentWithPipes(struct tokens* tokens, int start_index, int end_index,
+                                  int num_pipes, int pipes_array[][2], int process_index,
+                                  bool dup_stdin, bool dup_stdout) {
+  int segment_length = end_index - start_index;
+  if (segment_length == 0) {
+    return -1;
+  }
+
+  char* executable_path = tokens_get_token(tokens, start_index);
+  char* resolved_path_pass_through = executable_path;
+  bool needs_free = false;
+
+  // PATH to executable is not provided explicitly, resolve via PATH
+  if (access(executable_path, X_OK) == -1) {
+    char* path = getenv("PATH");
+    if (path) {
+      char* copied_path = strdup(path);
+      char* save_ptr;
+      char* dir = strtok_r(copied_path, ":", &save_ptr);
+      bool hasResolvedPath = false;
+      
+      while (dir != NULL) {
+        int resolved_path_length = strlen(dir) + 1 + strlen(executable_path);
+        char resolved_path[resolved_path_length + 1];
+        sprintf(resolved_path, "%s/%s", dir, executable_path);
+        
+        if (access(resolved_path, X_OK) == 0) {
+          hasResolvedPath = true;
+          resolved_path_pass_through = strdup(resolved_path);
+          needs_free = true;
+          break;
+        }
+        dir = strtok_r(NULL, ":", &save_ptr);
+      }
+      free(copied_path);
+      
+      if (!hasResolvedPath) {
+        printf("error: path could not be resolved for executable\n");
+        return -1;
+      }
+    } else {
+      printf("error: path could not be resolved for executable\n");
+      return -1;
+    }
+  }
+
+  // Parse arguments and standard I/O redirection
+  char* argv[segment_length + 1];
+  
+  int arg_idx = 0;
+  for (int i = start_index; i < end_index; i++) {
+    char* current_arg = tokens_get_token(tokens, i);
+    if (arg_idx == 0) {
+        argv[arg_idx++] = resolved_path_pass_through;
+    } else {
+      argv[arg_idx++] = current_arg;
+    }
+  }
+  argv[arg_idx] = NULL;
+
+  pid_t pid = fork();
+  if (pid == 0) {
+    // Retrieve the necessary FDs to dupe
+    if (dup_stdin) {
+      dup2(pipes_array[process_index - 1][0], STDIN_FILENO);
+    }
+    if (dup_stdout) {
+      dup2(pipes_array[process_index][1], STDOUT_FILENO);
+    }
+
+    // Close all pipes in child process
+    for (int i = 0; i < num_pipes; i++) {
+      close(pipes_array[i][0]);
+      close(pipes_array[i][1]);
+    }
+
+    // Apply I/O redirections from tokens, since they override pipes
+    // if (stdin_redirection != NULL) {
+    //   int new_stdin_redirect_fd = open(stdin_redirection, O_RDONLY);
+    //   if (new_stdin_redirect_fd >= 0) {
+    //     dup2(new_stdin_redirect_fd, 0);
+    //     close(new_stdin_redirect_fd);
+    //   }
+    // }
+
+    // if (stdout_redirection != NULL) {
+    //   int new_stdout_redirect_fd = open(stdout_redirection, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    //   if (new_stdout_redirect_fd >= 0) {
+    //     dup2(new_stdout_redirect_fd, 1);
+    //     close(new_stdout_redirect_fd);
+    //   }
+    // }
+
+    // Execute child process
+    execv(resolved_path_pass_through, argv);
+    printf("executing program caused error\n");
+    if (needs_free) free(resolved_path_pass_through);
+    exit(1);
+  } else if (pid > 0) {
+    // Parent process codepath
+    if (needs_free) free(resolved_path_pass_through);
+    return pid;
+  } else {
+    perror("fork error");
+    if (needs_free) free(resolved_path_pass_through);
+    return -1;
+  }
+}
+
+int runProgramWithPipes(struct tokens* tokens, int num_pipes) {
+  int pipes_array[num_pipes][2];
+
+  for (int i = 0; i < num_pipes; i++) {
+    int result = pipe(pipes_array[i]);
+    if (result == -1) {
+      perror("pipe");
+      return -1;
+    }
+  }
+
+  size_t curr_index = 0;
+  int current_process_start_index = 0;
+  size_t num_tokens = tokens_get_length(tokens);
+  
+  int process_index = -1;
+  pid_t pids[num_pipes + 1];
+
+  while (curr_index <= num_tokens) {
+    char* token = NULL;
+    if (curr_index < num_tokens) {
+      token = tokens_get_token(tokens, curr_index);
+    }
+    
+    if (curr_index == num_tokens || strcmp("|", token) == 0) {
+      process_index++;
+      bool dup_stdin = false;
+      bool dup_stdout = false;
+      
+      if (process_index == 0) {
+        dup_stdout = true;
+      } else if (process_index == num_pipes) {
+        dup_stdin = true;
+      } else {
+        dup_stdin = true;
+        dup_stdout = true;
+      }
+
+      pids[process_index] = execProgramSegmentWithPipes(
+        tokens, current_process_start_index, curr_index,
+        num_pipes, pipes_array, process_index,
+        dup_stdin, dup_stdout
+      );
+
+      current_process_start_index = curr_index + 1;
+    }
+    curr_index++;
+  }
+
+  // Close all FDs in the parent process outer function
+  for (int i = 0; i < num_pipes; i++) {
+    close(pipes_array[i][0]);
+    close(pipes_array[i][1]);
+  }
+
+  // Wait for all cloned child processes
+  for (int i = 0; i <= num_pipes; i++) {
+    if (pids[i] > 0) {
+      waitpid(pids[i], NULL, 0);
+    }
+  }
+
+  return 1;
+}
+
 /* Intialization procedures for this shell */
 void init_shell() {
   /* Our shell is connected to standard input. */
@@ -261,7 +435,25 @@ int main(unused int argc, unused char* argv[]) {
       cmd_table[fundex].fun(tokens);
     } else {
       /* REPLACE this to run commands as programs. */
-      runProgram(tokens);
+      // check if and how many pipes there are
+      // validate that there are n - 1 pipes for n processes we want to run
+      // call separate helper that runs these processes and passes the relevant tokens into executeProgramSegment to only 
+      // use the program name and its relevant args when calling execv
+      size_t num_tokens = tokens_get_length(tokens);
+      int num_pipes = 0;
+      for (size_t i = 0; i < num_tokens; i++) {
+        char* curr_token = tokens_get_token(tokens, i);
+        if (strcmp("|", curr_token) == 0) {
+          num_pipes++;
+        }
+      }
+      if (num_pipes != 0) {
+        // printf("Goes into pipes codepath\n");
+        runProgramWithPipes(tokens, num_pipes);
+      } else {
+        // printf("Goes into non-pipes codepath\n");
+        runProgram(tokens);
+      }
     }
 
     if (shell_is_interactive)
